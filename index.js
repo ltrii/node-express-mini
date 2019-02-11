80e403b604ca99921cfebcca67e9d02e5fb77147
ltrii <lelandtr@gmail.com>
2019-02-11 14:50:27 -0800

    Rewrote some for error checks

--- index.js
+++ index.js
@@ -6,39 +6,43 @@ const server = express();
 server.use(express.json());
 
 server.get('/api/users', (req, res) => {
-  db.find()
-    .then(users => {
-      res.status(200).json({ success: true, users });
-    })
-    .catch(err => {
-      res.status(err.code).json({ success: false, message: err.message });
-    });
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            res.status(500).json({error: "The users information could not be retrieved."});
+        });
 });
 
 server.post('/api/users', (req, res) => {
-  const user = req.body;
-
-  db
-    .insert(user)
-    .then(user => {
-      res.status(201).json({ success: true, user });
-    })
-    .catch(({ code, message }) => {
-      res.status(code).json({ success: false, message });
-    });
+    let user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({error: "Please provide name and bio for the user."});
+        return;
+    }
+    db.insert(user)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            res.status(500).json({error: "There was an error while saving the user to the database"});
+        });
 });
 
 server.get('/api/users/:id', (req, res) => {
-    const userId = req.params.id;
-
-    db.findById(userId)
-      .then(user => {
-        res.status(200).json({ success: true, user });
-      })
-      .catch(err => {
-        res.status(err.code).json({ success: false, message: err.message });
-      });
-  });
+    db.findById(req.params.id)
+        .then(user => {
+            if(!user) {
+                res.status(404).json({message: "The user with the specified ID does not exist."});
+            } else {
+                res.status(200).json(user);
+            }
+        })
+        .catch(err => {
+            res.status(500).json({error: "The user information could not be retrieved."});
+        });
+});
 
 server.delete('/api/users/:id', (req, res) => {
   const userId = req.params.id;
